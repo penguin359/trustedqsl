1937fe1e9e4df015352c53649654f3123a69ad8e
Hamish Moffatt <hamish@debian.org>
2004-08-15 16:35:07 +1000

    Import Debian changes 1.10-3
    trustedqsl (1.10-3) unstable; urgency=low
    
      * Fix section of package (libs -> hamradio) (closes: #243544)
      * Rebuild with latest libwxgtk2.4 (closes: #221357)
    
    trustedqsl (1.10-2) unstable; urgency=low
    
      * Add build-dep for zlib1g-dev (closes: #219058)
      * Renamed left and right xpms to resolve ambiguity problem
        at compile time (closes: #221538)
    
    trustedqsl (1.10-1) unstable; urgency=low
    
      * First upload

--- qsodatadialog.cpp
+++ qsodatadialog.cpp
@@ -291,13 +291,13 @@ QSODataDialog::QSODataDialog(wxWindow *parent, wxHtmlHelpController *help, QSORe
 		sizer = new wxBoxSizer(wxHORIZONTAL);
 		_recbottom_ctrl = new wxBitmapButton(this, QD_RECBOTTOM, wxBitmap(bottom), wxDefaultPosition, wxSize(18, TEXT_HEIGHT)),
 		sizer->Add(_recbottom_ctrl, 0, wxTOP|wxBOTTOM, 5);
-		_recdown_ctrl = new wxBitmapButton(this, QD_RECDOWN, wxBitmap(left), wxDefaultPosition, wxSize(18, TEXT_HEIGHT));
+		_recdown_ctrl = new wxBitmapButton(this, QD_RECDOWN, wxBitmap(leftxpm), wxDefaultPosition, wxSize(18, TEXT_HEIGHT));
 		sizer->Add(_recdown_ctrl, 0, wxTOP|wxBOTTOM, 5);
 		_recno_ctrl = new wxTextCtrl(this, QD_RECNO, "1", wxDefaultPosition,
 			wxSize(4*TEXT_WIDTH,TEXT_HEIGHT));
 		_recno_ctrl->Enable(FALSE);
 		sizer->Add(_recno_ctrl, 0, wxALL, 5);
-		_recup_ctrl = new wxBitmapButton(this, QD_RECUP, wxBitmap(right), wxDefaultPosition, wxSize(18, TEXT_HEIGHT));
+		_recup_ctrl = new wxBitmapButton(this, QD_RECUP, wxBitmap(rightxpm), wxDefaultPosition, wxSize(18, TEXT_HEIGHT));
 		sizer->Add(_recup_ctrl, 0, wxTOP|wxBOTTOM, 5);
 		_rectop_ctrl = new wxBitmapButton(this, QD_RECTOP, wxBitmap(top), wxDefaultPosition, wxSize(18, TEXT_HEIGHT)),
 		sizer->Add(_rectop_ctrl, 0, wxTOP|wxBOTTOM, 5);
